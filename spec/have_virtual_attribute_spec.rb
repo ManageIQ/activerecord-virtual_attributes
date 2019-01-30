d70e83599cd647e09c782a4b7edb9ae2cc82144b
Keenan Brock <keenan@thebrocks.net>
2019-01-30 10:58:38 -0500

    Rename have_virtual_column to have_virtual_attribute

--- spec/have_virtual_attribute_spec.rb
+++ spec/have_virtual_attribute_spec.rb
@@ -1,10 +1,10 @@
 describe "have_virtual_attribute" do
   it "detects virtual attribute" do
-    expect(Author).to have_virtual_column(:nick_or_name)
+    expect(Author).to have_virtual_attribute(:nick_or_name)
   end
 
   it "understands non virtual attributes" do
-    expect(Author).not_to have_virtual_column(:name)
-    expect(Author).not_to have_virtual_column(:elephant)
+    expect(Author).not_to have_virtual_attribute(:name)
+    expect(Author).not_to have_virtual_attribute(:elephant)
   end
 end
