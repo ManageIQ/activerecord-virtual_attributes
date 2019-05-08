39945d9612cf6fe0aef18460f56856f3c3ee6989
Keenan Brock <keenan@thebrocks.net>
2019-05-08 15:50:37 -0400

    virtual delegate cop

--- lib/active_record/virtual_attributes/virtual_delegates.rb
+++ lib/active_record/virtual_attributes/virtual_delegates.rb
@@ -152,11 +152,11 @@ module ActiveRecord
           # There is currently no way to propagate sql over a virtual association
           if reflect_on_association(to_ref.name) && (to_ref.macro == :has_one || to_ref.macro == :belongs_to)
             lambda do |t|
-              if ActiveRecord.version.to_s >= "5.1"
-                join_keys = to_ref.join_keys
-              else
-                join_keys = to_ref.join_keys(to_ref.klass)
-              end
+              join_keys = if ActiveRecord.version.to_s >= "5.1"
+                            to_ref.join_keys
+                          else
+                            to_ref.join_keys(to_ref.klass)
+                          end
               src_model_id = arel_attribute(join_keys.foreign_key, t)
               blk = ->(arel) { arel.limit = 1 } if to_ref.macro == :has_one
               VirtualDelegates.select_from_alias(to_ref, col, join_keys.key, src_model_id, &blk)
