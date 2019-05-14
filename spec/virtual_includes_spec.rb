b9af7c9a62d94b4ad78e470aa8b43be3c0a850d5
Keenan Brock <keenan@thebrocks.net>
2019-05-14 12:22:31 -0400

    use preload_values instead of match_query_limit
    did change include for virtual fields to use new association
    (this one is not as straight forward of a change as others)

--- spec/virtual_includes_spec.rb
+++ spec/virtual_includes_spec.rb
@@ -5,35 +5,39 @@ describe ActiveRecord::VirtualAttributes::VirtualIncludes do
     Author.create_with_books(3).books.first.create_bookmarks(2)
   end
 
+  let(:author_name) { "foo" }
+  # NOTE: each of the 1 authors has an array of books. so this value is [[Book, Book]]
+  let(:named_books) { [Book.where.not(:name => nil).order(:id).load] }
+
   context "virtual column" do
     it "as Symbol" do
-      expect { Author.includes(:nick_or_name).load }.to match_query_limit_of(1)
+      expect(Author.includes(:nick_or_name)).to preload_values(:nick_or_name, author_name)
     end
 
     it "as Array" do
-      expect { Author.includes([:nick_or_name]).load }.to match_query_limit_of(1)
-      expect { Author.includes([:nick_or_name, :bookmarks]).load }.to match_query_limit_of(3)
+      expect(Author.includes([:nick_or_name])).to preload_values(:nick_or_name, author_name)
+      expect(Author.includes([:nick_or_name, :bookmarks])).to preload_values(:nick_or_name, author_name)
     end
 
     it "as Hash" do
-      expect { Author.includes(:nick_or_name => {}).load }.to match_query_limit_of(1)
-      expect { Author.includes(:nick_or_name => {}, :bookmarks => :book).load }.to match_query_limit_of(4)
+      expect(Author.includes(:nick_or_name => {})).to preload_values(:nick_or_name, author_name)
+      expect(Author.includes(:nick_or_name => {}, :bookmarks => :book)).to preload_values(:nick_or_name, author_name)
     end
   end
 
   context "virtual reflection" do
     it "as Symbol" do
-      expect { Author.includes(:named_books).load }.to match_query_limit_of(2)
+      expect(Author.includes(:named_books)).to preload_values(:named_books, named_books)
     end
 
     it "as Array" do
-      expect { Author.includes([:named_books]).load }.to match_query_limit_of(2)
-      expect { Author.includes([:named_books, :bookmarks]).load }.to match_query_limit_of(3)
+      expect(Author.includes([:named_books])).to preload_values(:named_books, named_books)
+      expect(Author.includes([:named_books, :bookmarks])).to preload_values(:named_books, named_books)
     end
 
     it "as Hash" do
-      expect { Author.includes(:named_books => {}).load }.to match_query_limit_of(2)
-      expect { Author.includes(:named_books => {}, :bookmarks => :book).load }.to match_query_limit_of(4)
+      expect(Author.includes(:named_books => {})).to preload_values(:named_books, named_books)
+      expect(Author.includes(:named_books => {}, :bookmarks => :book)).to preload_values(:named_books, named_books)
     end
   end
 
@@ -43,39 +47,29 @@ describe ActiveRecord::VirtualAttributes::VirtualIncludes do
   end
 
   it "should handle virtual fields in :include when :conditions are also present in calculations" do
-    expect { Book.includes([:author_name, :author]).references(:author).where("authors.name = 'test'").count }.to match_query_limit_of(1)
-    expect { Book.includes([:author_name, :author]).references(:author).where("authors.id IS NOT NULL").count }.to match_query_limit_of(1)
+    expect(Book.includes([:author_name, :author]).references(:author).where("authors.name = 'test'")).to preload_values(:author_name, author_name)
+    expect(Book.includes([:author_name, :author]).references(:author).where("authors.id IS NOT NULL")).to preload_values(:author_name, author_name)
   end
 
   it "should fetch virtual fields without includes" do
-    book = nil
-    expect { book = Book.select(:author_name).first }.to match_query_limit_of(1)
-    expect { expect(book.author_name).to eq("foo") }.to match_query_limit_of(0)
+    expect(Book.select(:author_name)).to preload_values(:author_name, author_name)
   end
 
   it "should fetch virtual field using includes" do
-    book = nil
-    expect { book = Book.includes(:author_name).first }.to match_query_limit_of(2)
-    expect { expect(book.author_name).to eq("foo") }.to match_query_limit_of(0)
+    expect(Book.includes(:author_name)).to preload_values(:author_name, author_name)
   end
 
   it "should fetch virtual field using references" do
     skip("AR 5.1 not including properly") if ActiveRecord.version.to_s >= "5.1"
-    book = nil
-    expect { book = Book.includes(:author_name).references(:author_name).first }.to match_query_limit_of(2)
-    expect { expect(book.author_name).to eq("foo") }.to match_query_limit_of(0)
+    expect(Book.includes(:author_name).references(:author_name)).to preload_values(:author_name, author_name)
   end
 
   it "should fetch virtual field using all 3" do
     skip("AR 5.1 not including properly") if ActiveRecord.version.to_s >= "5.1"
-    book = nil
-    expect { book = Book.select(:author_name).includes(:author_name).references(:author_name).first }.to match_query_limit_of(2)
-    expect { expect(book.author_name).to eq("foo") }.to match_query_limit_of(0)
+    expect(Book.select(:author_name).includes(:author_name).references(:author_name)).to preload_values(:author_name, author_name)
   end
 
   it "should leverage include for virtual fields" do
-    authors = nil
-    expect { authors = Author.includes(:books => :author_name).load }.to match_query_limit_of(3)
-    expect { expect(authors.first.books.first.author_name).to eq(authors.first.name) }.to match_query_limit_of(0)
+    expect(Author.includes(:first_book_author_name => {})).to preload_values(:first_book_author_name, author_name)
   end
 end
