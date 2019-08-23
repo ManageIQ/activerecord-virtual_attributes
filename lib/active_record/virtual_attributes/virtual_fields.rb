5fdcb29f92758c0d9653443cd25482ce7a629f29
Keenan Brock <keenan@thebrocks.net>
2019-08-23 13:48:59 -0400

    remove single use variable: new_includes

--- lib/active_record/virtual_attributes/virtual_fields.rb
+++ lib/active_record/virtual_attributes/virtual_fields.rb
@@ -48,8 +48,7 @@ module ActiveRecord
         def replace_virtual_field_hash(associations)
           associations.each_with_object({}) do |(parent, child), h|
             if virtual_field?(parent) # form virtual_attribute => {}
-              new_includes = replace_virtual_fields(virtual_includes(parent))
-              merge_includes(h, new_includes)
+              merge_includes(h, replace_virtual_fields(virtual_includes(parent)))
             else
               reflection = reflect_on_association(parent.to_sym)
               new_child = reflection.nil? || reflection.options[:polymorphic] ? {} : reflection.klass.replace_virtual_fields(child) || {}
