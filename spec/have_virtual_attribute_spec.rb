eb90099d54bc0d053ac9433fc2ea0d5f36690061
Keenan Brock <keenan@thebrocks.net>
2019-08-23 11:02:51 -0400

    have_virtual_attributes code coverage

--- spec/have_virtual_attribute_spec.rb
+++ spec/have_virtual_attribute_spec.rb
@@ -3,8 +3,31 @@ describe "have_virtual_attribute" do
     expect(Author).to have_virtual_attribute(:nick_or_name, :string)
   end
 
-  it "understands non virtual attributes" do
+  it "detects virtual attribute failure" do
+    expect do
+      expect(Author).not_to have_virtual_attribute(:nick_or_name, :string)
+    end.to raise_error(RSpec::Expectations::ExpectationNotMetError, /not have virtual column/)
+  end
+
+  it "detects incorrect type" do
+    expect(Author).not_to have_virtual_attribute(:nick_or_name, :integer)
+  end
+
+  it "detects incorrect type failure" do
+    expect do
+      expect(Author).to have_virtual_attribute(:nick_or_name, :integer)
+    end.to raise_error(RSpec::Expectations::ExpectationNotMetError, /to have virtual column/)
+  end
+
+  it "detects virtual attribute" do
+    expect(Author).to have_virtual_attribute(:nick_or_name, :string)
+  end
+
+  it "detects an attribute is not virtual" do
     expect(Author).not_to have_virtual_attribute(:name, :string)
-    expect(Author).not_to have_virtual_attribute(:elephant)
+  end
+
+  it "detects missing virtual attribute" do
+    expect(Author).not_to have_virtual_attribute(:elephant, :string)
   end
 end
