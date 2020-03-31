c2167c764d180414256b546cf6b08313be857aa0
Keenan Brock <keenan@thebrocks.net>
2020-03-31 15:02:35 -0400

    Version check for grouping monkey patch
    We had a temporary patch for grouping while a pr was merged
    It has been incorporated in rails 6.0, so updating the patch
    to let us know this and when we can delete it

--- lib/active_record/virtual_attributes/arel_groups.rb
+++ lib/active_record/virtual_attributes/arel_groups.rb
@@ -1,6 +1,7 @@
 # this is from https://github.com/rails/arel/pull/435
 # this allows sorting and where clauses to work with virtual_attribute columns
-if defined?(Arel::Nodes::Grouping)
+# no longer needed for rails 6.0 and up (change was merged)
+if ActiveRecord.version.to_s < "6.0" && defined?(Arel::Nodes::Grouping)
   module Arel
     module Nodes
       class Grouping
