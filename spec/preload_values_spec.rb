fef10e1fa99340c3b2cabbca36e55c1f621ba8ca
Keenan Brock <keenan@thebrocks.net>
2023-04-30 18:36:56 -0400

    changed test verbiage around preloading values failures.
    There end up being a few double negatives.
    One example being that we expected the value not to be preloaded but it was
    so it is a failure of a not preloading
    
    We don't have a way to expect preloading (not preloading) to a failed value.

--- spec/preload_values_spec.rb
+++ spec/preload_values_spec.rb
@@ -6,39 +6,38 @@ RSpec.describe "preloads_values" do
   let(:author_name) { "foo" }
   let(:book_name) { "bar" }
 
-  it "detects preloaded values" do
+  it "detects values preloaded with a value" do
     expect(Book.select(:author_name)).to preload_values(:author_name, [author_name, author_name])
   end
 
-  it "detects preloaded values converts to array" do
+  it "detects values preloaded (auto converts value to an array)" do
     expect(Book.select(:author_name)).to preload_values(:author_name, author_name)
   end
 
-  it "detects not preloaded" do
+  it "detects values preloading failure" do
     expect do
       expect(Book).to preload_values(:author_name, author_name)
     end.to raise_error(RSpec::Expectations::ExpectationNotMetError, "Expected to preload author_name but executed 2 queries instead")
   end
 
-  it "detects incorrect values" do
+  it "detects values matching failure" do
     expect do
       expect(Book.select(:author_name)).to preload_values(:author_name, "bogus")
-    end.to raise_error(RSpec::Expectations::ExpectationNotMetError)
+    end.to raise_error(RSpec::Expectations::ExpectationNotMetError, /expected:/)
   end
 
-  it "detects not preloading values" do
+  it "detects values not preloaded" do
     expect(Book).not_to preload_values(:author_name, author_name)
   end
 
-  # NOTE: was unsure if incorrect values met or failed the expectation
-  # went with the core of the expectation is preloading. but matching values trumps all
-  it "detects not preloading values (bad value)" do
+  # even though we said not to preload, still expecting values to match
+  it "detects values not preloaded but matching failures" do
     expect do
       expect(Book).not_to preload_values(:author_name, "bogus")
-    end.to raise_error(RSpec::Expectations::ExpectationNotMetError)
+    end.to raise_error(RSpec::Expectations::ExpectationNotMetError, /expected:/)
   end
 
-  it "detects not preloading values failure" do
+  it "detects values not preloaded failure (expecting not preloaded but they were)" do
     expect do
       expect(Book.select(:author_name)).not_to preload_values(:author_name, author_name)
     end.to raise_error(RSpec::Expectations::ExpectationNotMetError, "Unexpectedly preloaded author_name")
