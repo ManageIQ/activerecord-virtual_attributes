f88399e19dd569781fce3a9f1e321dc00f8adeca
Keenan Brock <keenan@thebrocks.net>
2026-02-09 20:55:42 -0500

    Introduce rake db:create and db:drop
    When setting up the environment, it makes it easier to create the postgres and mysql databases

--- spec/support/database.rb
+++ spec/support/database.rb
@@ -21,21 +21,38 @@ class Database
     @dirname = "#{File.dirname(__FILE__)}/../db"
   end
 
-  def setup
-    if defined?(I18n)
-      I18n.enforce_available_locales = false if I18n.respond_to?(:enforce_available_locales=)
-      # I18n.fallbacks = [I18n.default_locale] if I18n.respond_to?(:fallbacks=)
-    end
-    @connection_options = YAML.safe_load(ERB.new(File.read("#{dirname}/database.yml")).result)[self.class.adapter]
+  def connection_options
+    @connection_options ||=
+      begin
+        if defined?(I18n)
+          I18n.enforce_available_locales = false if I18n.respond_to?(:enforce_available_locales=)
+          # I18n.fallbacks = [I18n.default_locale] if I18n.respond_to?(:fallbacks=)
+        end
+        YAML.safe_load(ERB.new(File.read("#{dirname}/database.yml")).result)[self.class.adapter]
+      end
+  end
 
+  def create
+    ActiveRecord::Base.establish_connection(connection_options.except("database"))
+    ActiveRecord::Base.connection.create_database(connection_options["database"])
+    self
+  end
+
+  def drop
+    ActiveRecord::Base.establish_connection(connection_options.except("database"))
+    ActiveRecord::Base.connection.drop_database(connection_options["database"])
+    self
+  end
+
+  def connect
+    ActiveRecord::Base.establish_connection(connection_options)
+    ActiveRecord::Base.connection # Check the connection works
     self
   end
 
   def migrate
     ActiveRecord::Migration.verbose = false
-    ActiveRecord::Base.establish_connection(@connection_options)
-    ActiveRecord::Base.connection # Check the connection works
-
+    connect
     require "#{dirname}/schema"
     require "#{dirname}/models"
 
